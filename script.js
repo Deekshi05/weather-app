68133bb98f5d0238ada7c4a0580afe52e26f845f
Deekshi05 <deekshithavemula50@gmail.com>
2024-09-03 22:31:05 +0530

    added more features

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ let apikey= "38648ccd248445265b1c7aa80c33c10a";
 let apiurl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 let searchbox=document.querySelector(".search input");
 let searchbtn=document.querySelector(".search button");
+let icon=document.querySelector(".weather-icon");
 
-
-async function checkweather(city){
-  let response=await fetch(apiurl + city + `&appid=${apikey}`);
+async function checkweather(ncity){
+  let response=await fetch(apiurl + ncity + `&appid=${apikey}`);
   let data=await response.json();
   console.log(data);
-  let city=document.querySelector(".city");
-  city.innerHTML=data.name;
-  let temp=document.querySelector(".temp");
-  temp.innerHTML=Math.round(data.main.temp)+"°C";
-  let humidity=document.querySelector(".humidity");
-  humidity.innerHTML=data.main.humidity+"%";
-let wind=document.querySelector(".wind");
-wind.innerHTML=data.wind.speed+"km/hr";
+  document.querySelector(".city").innerHTML=data.name;
+  document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+  document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+  document.querySelector(".wind").innerHTML=data.wind.speed+"km/hr";
+  if(data.weather[0].main=="Clouds"){
+      icon.src="clouds.png";
+  }
+  else if(data.weather[0].main=="Clear"){
+    icon.src="clear.png";
+}
+else if(data.weather[0].main=="Rain"){
+  icon.src="rain.png";
+}
+else if(data.weather[0].main=="Drizzle"){
+  icon.src="drizzle.png";
+}
+else if(data.weather[0].main=="Mist"){
+  icon.src="mist.png";
+}
 }
 
 
