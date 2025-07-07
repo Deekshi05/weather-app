4eee10a43aa914ac4cb04ab2b23723c97974b23b
Deekshi05 <163875958+Deekshi05@users.noreply.github.com>
2025-07-07 19:15:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-let apikey= "38648ccd248445265b1c7aa80c33c10a";
-let apiurl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-let searchbox=document.querySelector(".search input");
-let searchbtn=document.querySelector(".search button");
-let icon=document.querySelector(".weather-icon");
-
 async function checkweather(ncity){
-  let response=await fetch(apiurl + ncity + `&appid=${apikey}`);
-  let data=await response.json();
-  console.log(data);
-  document.querySelector(".city").innerHTML=data.name;
-  document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
-  document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-  document.querySelector(".wind").innerHTML=data.wind.speed+"km/hr";
-  if(data.weather[0].main=="Clouds"){
-      icon.src="clouds.png";
+  try {
+    let response = await fetch(apiurl + ncity + `&appid=${apikey}`);
+    if (!response.ok) {
+      alert("City not found. Please try again.");
+      return;
+    }
+    let data = await response.json();
+    
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+
+    if (data.weather[0].main == "Clouds") {
+      icon.src = "clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      icon.src = "clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      icon.src = "rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      icon.src = "drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      icon.src = "mist.png";
+    }
+    
+  } catch (error) {
+    alert("An error occurred. Please try again.");
   }
-  else if(data.weather[0].main=="Clear"){
-    icon.src="clear.png";
-}
-else if(data.weather[0].main=="Rain"){
-  icon.src="rain.png";
-}
-else if(data.weather[0].main=="Drizzle"){
-  icon.src="drizzle.png";
-}
-else if(data.weather[0].main=="Mist"){
-  icon.src="mist.png";
-}
 }
 
-
-searchbtn.addEventListener("click",()=>{
+searchbox.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
     checkweather(searchbox.value);
-})
+  }
+});
